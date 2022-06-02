60e7f99170f90f544256dcdecb3602c36b0f93c8
BlaskoMate <blaskomate3@gmail.com>
2022-06-02 13:16:27 +0200

    FEAT: add implementation to save the response logs to CSV file

--- src/main/java/dao/implementation/LoggerDaoMem.java
+++ src/main/java/dao/implementation/LoggerDaoMem.java
@@ -2,6 +2,12 @@ package dao.implementation;
 
 
 import dao.LoggerDao;
+import util.ResponseObj;
+import util.Util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class LoggerDaoMem implements LoggerDao {
 
@@ -29,6 +35,36 @@ public class LoggerDaoMem implements LoggerDao {
             instance = new LoggerDaoMem();
         }
         return instance;
+    }
+
+    public void printRecentLogsToConsole(){
+        for (String row : logger){
+            System.out.println(row);
+        }
+    }
+
+    public void addLog(ResponseObj responseObj, boolean append){
+        String log = formatLog(responseObj);
+        logger = Arrays.copyOf(logger, logger.length + 1);
+        logger[logger.length - 1] = log;
+        System.out.println(writeLogToFile(log, append));
+    }
+
+    public String writeLogToFile(String log, boolean append){
+        Util.writeToCSVFile(LOGGER_FILE_PATH, log, append);
+        return String.format("Writing \"%s\" to filePath: %s",
+                log, LOGGER_FILE_PATH);
+    }
+
+    public String formatLog(ResponseObj responseObj){
+        LocalDateTime date = responseObj.getDate();
+        int status = responseObj.getStatus();
+        String message = responseObj.getMessage();
+        String url = responseObj.getUrl();
+        String body = responseObj.getBody();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+
+        return String.format("\n%s,%s,%s,%s,%s", dateFormat.format(date), status, message, url, body);
     }
 
 }
