99eee70b67672f1d694508897133a5e3ea2b6a58
BlaskoMate <blaskomate3@gmail.com>
2022-06-03 01:50:03 +0200

    FEAT: add entry point to the application

---
+++ src/main/java/mate/blasko/apihelper/controller/Main.java
@@ -0,0 +1,11 @@
+package mate.blasko.apihelper.controller;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        MyApp app = new MyApp();
+        app.run(args);
+    }
+}
