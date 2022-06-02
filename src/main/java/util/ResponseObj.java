188b033310a60f3f38af2eeadba122caa6c5b708
BlaskoMate <blaskomate3@gmail.com>
2022-06-02 13:05:12 +0200

    FEAT: add base structure to ResponseObj.java

--- src/main/java/util/ResponseObj.java
+++ src/main/java/util/ResponseObj.java
@@ -1,5 +1,22 @@
 package util;
 
-public class ResponseObj{
+public class ResponseObj extends RequestObj{
+
+    private int status;
+    private String message;
+
+    public ResponseObj(RequestObj requestObj, int status, String message) {
+        super(requestObj.getUrl(), requestObj.getBody(), requestObj.getDate());
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 
 }
