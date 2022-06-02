35d86f3adcf3bc28766a969f91f0863047864a46
BlaskoMate <blaskomate3@gmail.com>
2022-06-02 12:48:26 +0200

    FEAT: add base structure to ApiPostRequestSender.java

--- src/main/java/controller/ApiPostRequestSender.java
+++ src/main/java/controller/ApiPostRequestSender.java
@@ -1,6 +1,24 @@
 package controller;
 
+import dao.implementation.LoggerDaoMem;
 
 public class ApiPostRequestSender {
+
+
+    private LoggerDaoMem logger;
+    //  When false the csv file gets overwritten
+    private final boolean APPEND_FILE;
+    private static final String CSV_DELIMITER = ",";
+
+
+    public ApiPostRequestSender(){
+        this.logger = LoggerDaoMem.getInstance();
+        APPEND_FILE = true;
+    }
+
+    public ApiPostRequestSender(boolean append){
+        this.logger = LoggerDaoMem.getInstance();
+        APPEND_FILE = append;
+    }
 
 }
