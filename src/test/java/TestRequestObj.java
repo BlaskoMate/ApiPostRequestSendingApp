ac693ef9ea5da57799b524f78924669746441aa7
BlaskoMate <blaskomate3@gmail.com>
2022-06-03 01:42:17 +0200

    FEAT: add test case creating RequestObj with correct body syntax

---
+++ src/test/java/TestRequestObj.java
@@ -0,0 +1,15 @@
+import mate.blasko.apihelper.util.RequestObj;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestRequestObj {
+
+    @Test
+    public void TestRequestObjCreationWithCorrectBodySyntax(){
+        ArrayList<String> requestInfo = new ArrayList<>(Arrays.asList("url", "a1:b2", "c3:d4"));
+        Assertions.assertEquals(new RequestObj(requestInfo).getBody(), "{\"a1\":\"b2\",\"c3\":\"d4\"}");
+    }
+}
