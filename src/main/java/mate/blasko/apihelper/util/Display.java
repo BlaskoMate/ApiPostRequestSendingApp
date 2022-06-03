c1281bc933d67e0bf051246622ebaf7a9075d4f8
BlaskoMate <blaskomate3@gmail.com>
2022-06-03 02:32:49 +0200

    FEAT: add basic Display.java

---
+++ src/main/java/mate/blasko/apihelper/util/Display.java
@@ -0,0 +1,27 @@
+package mate.blasko.apihelper.util;
+
+import mate.blasko.apihelper.dao.mem.LoggerDaoMem;
+
+import java.util.ArrayList;
+
+public class Display {
+
+    public static void printLogsToConsole(ArrayList<String> logs){
+        for (String log : logs){
+            System.out.println(log);
+        }
+    }
+
+    public static void printHelp() {
+        System.out.println(LoggerDaoMem.getInstance().LOGGER_FILE_PATH + " is set as default Logger file path.");
+    }
+
+    public static void printMenu() {
+        System.out.println("1) send post request\n2) send multiple post requests from file\n3) print recently sent requests\n4) print all sent requests\n5) exit");
+    }
+
+
+    public static void clearConsole(){
+        // TODO:
+    }
+}
